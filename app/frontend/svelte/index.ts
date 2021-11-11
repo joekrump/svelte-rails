0ce74e11d3a6bd2ecde0dae489f60e41bb3f3426
Joe Krump <joe.krump@clio.com>
2021-11-11 18:09:41 -0500

    Rename test.svelte -> hello.svelte

--- app/frontend/svelte/index.ts
+++ app/frontend/svelte/index.ts
@@ -1,9 +1,9 @@
 import ViteSvelte from "./helper";
 
-import Test from "./components/test.svelte";
+import Hello from "./components/hello.svelte";
 import Home from "./views/home.svelte";
 
 ViteSvelte.setup({
   Home,
-  Test,
+  Hello,
 });
