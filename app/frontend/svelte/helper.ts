0b7d5ea33be3448a959eead63fd7f8b6efb1c659
Joe Krump <joe.krump@clio.com>
2022-06-02 13:25:09 -0400

    Replace turbolinks support with turbo support

--- app/frontend/svelte/helper.ts
+++ app/frontend/svelte/helper.ts
@@ -5,7 +5,7 @@
 // which is distributed under Apache License 2.0
 
 interface BrowserWindow extends Window {
-
+  Turbo?: unknown;
 }
 
 const ujs = {
@@ -14,67 +14,22 @@ const ujs = {
   },
 
   setup(onMount, onUnmount) {
-    const $ = typeof window.jQuery !== 'undefined' && window.jQuery;
-    const { Turbolinks } = window;
+    const { Turbo } = window as BrowserWindow;
 
-    // Detect which kind of events to set up:
-    if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
-      if (typeof Turbolinks.EVENTS !== 'undefined') {
-        // Turbolinks.EVENTS is in classic version 2.4.0+
-        this.turbolinksClassic(onMount, onUnmount);
-      } else if (typeof Turbolinks.controller !== 'undefined') {
-        // Turbolinks.controller is in version 5+
-        this.turbolinks5(onMount, onUnmount);
-      } else {
-        this.turbolinksClassicDeprecated(onMount, onUnmount);
-      }
-    } else if ($ && typeof $.pjax === 'function') {
-      this.pjax(onMount, onUnmount);
-    } else {
-      this.native(onMount);
+    if (typeof Turbo !== "undefined") {
+      this.turboDrive(onMount, onUnmount)
     }
+    this.native(onMount);
   },
 
-  turbolinks5(onMount, onUnmount) {
-    this.handleEvent('turbolinks:load', onMount, { once: true });
-    this.handleEvent('turbolinks:render', onMount);
-    this.handleEvent('turbolinks:before-render', onUnmount);
-  },
-
-  turbolinksClassic(onMount, onUnmount) {
-    const { Turbolinks } = window;
-
-    this.handleEvent(Turbolinks.EVENTS.CHANGE, onMount);
-    this.handleEvent(Turbolinks.EVENTS.BEFORE_UNLOAD, onUnmount);
-  },
-
-  turbolinksClassicDeprecated(onMount, onUnmount) {
-    // Before Turbolinks 2.4.0, Turbolinks didn't
-    // have named events and didn't have a before-unload event.
-    // Also, it didn't work with the Turbolinks cache, see
-    // https://github.com/reactjs/react-rails/issues/87
-    const { Turbolinks } = window;
-    Turbolinks.pagesCached(0);
-    this.handleEvent('page:change', onMount);
-    this.handleEvent('page:receive', onUnmount);
-  },
-
-  pjax(onMount, onUnmount) {
-    this.handleEvent('ready', onMount);
-    this.handleEvent('pjax:end', onMount);
-    this.handleEvent('pjax:beforeReplace', onUnmount);
+  turboDrive(onMount, onUnmount) {
+    this.handleEvent('turbo:load', onMount, { once: true });
+    this.handleEvent('turbo:render', onMount);
+    this.handleEvent('turbo:before-render', onUnmount);
   },
 
   native(onMount) {
-    const $ = typeof window.jQuery !== 'undefined' && window.jQuery;
-    if ($) {
-      $(() => onMount());
-    } else if ('addEventListener' in window) {
-      document.addEventListener('DOMContentLoaded', onMount);
-    } else {
-      // add support to IE8 without jQuery
-      window.attachEvent('onload', onMount);
-    }
+    document.addEventListener('DOMContentLoaded', onMount);
   }
 };
 
@@ -106,9 +61,10 @@ const ViteSvelte = {
     // );
     // if (collisions.length > 0) {
     //   console.error(
-    //     `webpacker-svelte: can not register components. Following components are already registered: ${collisions}`
+    //     `vite-svelte: can not register components. Following components are already registered: ${collisions}`
     //   );
     // }
+
     this.registeredComponents = components;
 
     // assign(this.registeredComponents, omit(components, collisions));
